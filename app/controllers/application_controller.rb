7375f79762ef90ddf4e7229b5c2935b916250db3
Lelion01 <leonnel.noundoutchounga@cgi.com>
2018-06-13 17:01:54 -0400

    add sign-in/sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-	before_action :set_locale
+	
+  protect_from_forgery with: :exception
+  before_action :set_current_user
+
+  before_action :set_locale
+  before_action :authenticate_user!
+
+  
 
 	def set_locale
 		I18n.locale =  params[:locale] || I18n.default_locale
@@ -12,4 +19,12 @@ class ApplicationController < ActionController::Base
 	def default_url_options(options = {})
 		{locale: I18n.locale}
 	end
+
+	private
+
+    def set_current_user
+      if session[:user_id]
+        @current_user = User.find(session[:user_id])
+      end
+    end
 end
